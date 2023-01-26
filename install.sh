e2161038d3cf8475c5df4b58267a3712b7fc7597
Lily Dayton <lily.dayton.git@gmail.com>
2023-01-26 08:47:23 -0500

    improve install script

--- install.sh
+++ install.sh
@@ -1,22 +1,44 @@
 #!/bin/sh
 
-if [ -e ${CODESPACE} ]; then
+set -ex
+
+#--- Where is DOT_DIR? ---#
+if [ -e "${CODESPACE}" ]; then
     DOT_DIR='/workspaces/.codespaces/.persistedshare/dotfiles'
 fi
 
-if [ `uname` == 'Darwin' ]; then
+if [ "$(uname)" = 'Darwin' ]; then
     DOT_DIR="${HOME}/source/dotfiles"
 fi
 
-rm -rf "${HOME}/.zshrc"; ln -s "${DOT_DIR}/zshrc" "${HOME}/.zshrc"
-rm -rf "${HOME}/.gitconfig"; ln -s "${DOT_DIR}/gitconfig" "${HOME}/.gitconfig"
-rm -rf "${HOME}/.ackrc"; ln -s "${DOT_DIR}/ackrc" "${HOME}/.ackrc"
+#--- Backup and link dot files ---#
+if [ -f "${HOME}/.zshrc" ]; then
+  cp "${HOME}/.zshrc" "${HOME}/.zshrc.bak"
+  rm -rf "${HOME}/.zshrc";
+fi
+ln -s "${DOT_DIR}/zshrc" "${HOME}/.zshrc"
 
-if [ -e ${CODESPACE} ]; then
+if [ -f "${HOME}/.gitconfig" ]; then
+  cp "${HOME}/.gitconfig" "${HOME}/.gitconfig.bak";
+  rm -rf "${HOME}/.gitconfig";
+fi
+ln -s "${DOT_DIR}/gitconfig" "${HOME}/.gitconfig";
+
+if [ -f "${HOME}/.ackrc" ]; then
+  cp "${HOME}/.ackrc" "${HOME}/.ackrc.bak";
+  rm -rf "${HOME}/.ackrc";
+fi
+ln -s "${DOT_DIR}/ackrc" "${HOME}/.ackrc"
+
+#--- Homebrew bundle ---#
+if [ -e "${CODESPACE}" ]; then
   brew bundle --file="${DOT_DIR}/brewfile.codespaces"
 fi
 
-if [ `uname` == 'Darwin' ]; then
+if [ "$(uname)" = 'Darwin' ]; then
   brew bundle --file="${DOT_DIR}/brewfile.darwin"
 fi
+
+brew update;
+brew upgrade;
 
